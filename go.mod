4b1e8b23f63cb6d982b7846349acd986d618db05
Huakun Shen <huakun.shen@huakunshen.com>
2022-11-08 03:45:14 -0500

    Started to work on golang kafka client but mac doesn't work

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/CrossCopy/crosscopy-mq-notification-service
 
 go 1.19
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+	github.com/confluentinc/examples v7.3.0+incompatible // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
-import "github.com/CrossCopy/crosscopy-mq-notification-service/notification_service"
+import (
+	"fmt"
+	"github.com/confluentinc/examples/clients/cloud/go/ccloud"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"os"
+)
 
 func main() {
-	ret, err := notification_service.ConstructSignupEmailVerificationRedisKey("dev", "dev@crosscopy.io", "1024")
-	ret, err = notification_service.ConstructSignupEmailVerificationRedisKey("", "dev@crosscopy.io", "1024")
+	configFile, topic := ccloud.ParseArgs()
+	conf := ccloud.ReadCCloudConfig(*configFile)
+	// Create Consumer instance
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": conf["bootstrap.servers"],
+		"sasl.mechanisms":   conf["sasl.mechanisms"],
+		"security.protocol": conf["security.protocol"],
+		"sasl.username":     conf["sasl.username"],
+		"sasl.password":     conf["sasl.password"],
+		"group.id":          "go_example_group_1",
+		"auto.offset.reset": "earliest",
+	})
 	if err != nil {
-		panic(err)
+		fmt.Printf("Failed to create consumer: %s", err)
+		os.Exit(1)
 	}
-	println(ret)
+
+	// Subscribe to topic
+	err = consumer.SubscribeTopics([]string{*topic}, nil)
+
+	//ret, err := notification_service.ConstructSignupEmailVerificationRedisKey("dev", "dev@crosscopy.io", "1024")
+	//ret, err = notification_service.ConstructSignupEmailVerificationRedisKey("", "dev@crosscopy.io", "1024")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//println(ret)
 
 }
